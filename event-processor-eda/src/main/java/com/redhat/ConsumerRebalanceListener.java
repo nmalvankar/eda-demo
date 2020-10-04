520dd3ffb0a9908cb678cf1b598883d8be3c680a
Nikhil Malvankar <nmalvank@Nikhils-MacBook-Pro.local>
2020-10-04 18:11:50 -0400

    added rebalancer

--- event-processor-eda/src/main/java/com/redhat/ConsumerRebalanceListener.java
+++ event-processor-eda/src/main/java/com/redhat/ConsumerRebalanceListener.java
@@ -1,83 +1,54 @@
+package com.redhat;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+
+import org.jboss.logging.Logger;
+
+import io.smallrye.mutiny.Uni;
+import io.smallrye.reactive.messaging.kafka.KafkaConsumerRebalanceListener;
+import io.vertx.kafka.client.common.TopicPartition;
+import io.vertx.mutiny.kafka.client.consumer.KafkaConsumer;
+
 @ApplicationScoped
 @Named("txn-from-kafka.rebalancer")
-public class KafkaRebalancedConsumerRebalanceListener implements KafkaRebalancedConsumerRebalanceListener {
-
+public class ConsumerRebalanceListener implements KafkaConsumerRebalanceListener {
+	
+	private static final Logger LOGGER = Logger.getLogger("ConsumerRebalanceListener");
+	
+	
+	
     @Override
-    public void onPartitionsRevoked(Collection<TopicPartition> partitions) {
-
+    public Uni<Void> onPartitionsAssigned(KafkaConsumer<?, ?> consumer, Set<TopicPartition> topicPartitions) {
+        
+        return Uni
+            .combine()
+            .all()
+            .unis(topicPartitions
+                .stream()
+                .map(topicPartition -> {
+                    LOGGER.info("Assigned " + topicPartition);
+                    return consumer.committed(topicPartition)
+                        .onItem()
+                        .invoke(o -> LOGGER.info("Seeking to " + o))
+                        .onItem()
+                        .produceUni(o -> consumer
+                            .seek(topicPartition, o == null ? 0L : o.getOffset())
+                            .onItem()
+                            .invoke(v -> LOGGER.info("Seeked to " + o))
+                        );
+                })
+                .collect(Collectors.toList()))
+            .combinedWith(a -> null);
     }
 
-    @Override
-    public void onPartitionsAssigned(Collection<TopicPartition> partitions) {
-        List<Map<TopicPartition, OffsetAndMetadata>> mirroredOffsets = getMirroredOffsets();
-
-        for (TopicPartition partition : partitions) {
-            OffsetAndMetadata mirroredOffset = findHighestMirroredOffset(mirroredOffsets, partition);
-            OffsetAndMetadata localOffset = consumer.committed(Collections.singleton(partition)).get(partition);
-
-            if (mirroredOffset != null) {
-                if (localOffset != null)    {
-                    if (mirroredOffset.offset() > localOffset.offset()) {
-                        LOG.warn("Seeking to {} in {} (higher than local offset {})", mirroredOffset.offset(), partition, localOffset.offset());
-                        consumer.seek(partition, mirroredOffset);
-                    } else {
-                        LOG.warn("Keeping local offset {} in {} (higher than mirrored offset {})", localOffset.offset(), partition, mirroredOffset.offset());
-                    }
-                } else {
-                    LOG.warn("Seeking to {} in {} (local offset does not exist)", mirroredOffset.offset(), partition);
-                    consumer.seek(partition, mirroredOffset);
-                }
-            } else {
-                LOG.warn("Mirrored offset does not exist for partition {}", partition);
-            }
-        }
-    }
-
-    public OffsetAndMetadata findHighestMirroredOffset(List<Map<TopicPartition, OffsetAndMetadata>> mirroredOffsets, TopicPartition partition) {
-        OffsetAndMetadata foundOffset = null;
-
-        for (Map<TopicPartition, OffsetAndMetadata> offsets : mirroredOffsets)  {
-            if (offsets.containsKey(partition)) {
-                if (foundOffset == null)    {
-                    foundOffset = offsets.get(partition);
-                } else  {
-                    OffsetAndMetadata newOffset = offsets.get(partition);
-                    if (foundOffset.offset() < newOffset.offset())   {
-                        foundOffset = newOffset;
-                    }
-                }
-            }
-        }
-
-        return foundOffset;
-    }
-
-    public List<Map<TopicPartition, OffsetAndMetadata>> getMirroredOffsets() {
-        Set<String> clusters = null;
-        try {
-            clusters = RemoteClusterUtils.upstreamClusters(props);
-        } catch (InterruptedException e) {
-            LOG.error("Failed to get remote cluster", e);
-            return Collections.emptyList();
-        } catch (TimeoutException e) {
-            LOG.error("Failed to get remote cluster", e);
-            return Collections.emptyList();
-        }
-
-        List<Map<TopicPartition, OffsetAndMetadata>> mirroredOffsets = new ArrayList<>();
-
-        for (String cluster : clusters) {
-            try {
-                mirroredOffsets.add(RemoteClusterUtils.translateOffsets(props, cluster, props.get(ConsumerConfig.GROUP_ID_CONFIG).toString(), Duration.ofMinutes(1)));
-            } catch (InterruptedException e) {
-                LOG.error("Failed to translate offsets", e);
-                e.printStackTrace();
-            } catch (TimeoutException e) {
-                LOG.error("Failed to translate offsets", e);
-            }
-        }
-
-        return mirroredOffsets;
-    }
-
-}    
+	@Override
+	public Uni<Void> onPartitionsRevoked(KafkaConsumer<?, ?> arg0, Set<TopicPartition> arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
