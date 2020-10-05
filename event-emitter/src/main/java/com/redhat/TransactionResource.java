0e34392313c2e1e7ce9a488f578dc54bbc7ea82a
Nikhil Malvankar <nmalvank@Nikhils-MacBook-Pro.local>
2020-10-04 23:02:05 -0400

    fix avro schema

--- event-emitter/src/main/java/com/redhat/TransactionResource.java
+++ event-emitter/src/main/java/com/redhat/TransactionResource.java
@@ -11,6 +11,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import com.redhat.Transaction;
 
 @Path("/transaction")
 @Consumes(MediaType.APPLICATION_JSON)
